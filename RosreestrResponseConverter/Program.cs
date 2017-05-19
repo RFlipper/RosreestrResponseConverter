a384fa174374e56fdec0e847e69df9ebd4cff1c2
ivsemionov <ivsemionov@iggn.permkrai.ru>
2017-05-19 10:14:53 +0500

    Даунгрейд до .NETFramework v4.0

--- RosreestrResponseConverter/Program.cs
+++ RosreestrResponseConverter/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
